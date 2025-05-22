af2d63d6894cf29300a317d87f847d9a59ff0b8b
Lokeshranjan8 <lokesh.ranjan.phe23@itbhu.ac.in>
2025-05-22 18:06:27 +0530

    main-complet

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,7 @@
 export default function Content() {
     return (
-
-        <div className="bg-blue-500  text-white  border-red-950  ">
-            <h1>MY ---  Content</h1>
-            <div className="bg-blue-500  text-white  border-red-950  ">
-            <h1>MY ---  Content</h1>
-        </div>
         <div className="bg-blue-500  text-white  border-red-950  ">
             <h1>MY ---  Content</h1>
         </div>
-        </div>
-
     )
-
 }
