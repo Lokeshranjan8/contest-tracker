7188c8022157a4f028d0d05f33b5687ea20adee3
Lokeshranjan8 <lokesh.ranjan.phe23@itbhu.ac.in>
2025-07-07 19:06:17 +0530

    upstash connect with redis

--- server/redis.js
+++ server/redis.js
@@ -1,7 +1,11 @@
 import {createClient } from 'redis';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const redisClient = createClient ();
-
+const redisClient = createClient({
+  // eslint-disable-next-line no-undef
+  url: process.env.REDIS_URL,
+});
 redisClient.on('error',(err) =>{
     console.error('Redis Client Error', err);
 });
