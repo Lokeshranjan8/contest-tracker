92ae70576a193320e0cbdece231dfbfc3fb813e8
Lokeshranjan8 <lokesh.ranjan.phe23@itbhu.ac.in>
2025-07-07 20:13:53 +0530

    render-db

--- server/db.js
+++ server/db.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.PG_RENDER,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+    
 })
 
 try{
