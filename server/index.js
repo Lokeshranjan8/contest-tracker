e0e12113d5da811d361f9f0a818da589c478d221
Lokeshranjan8 <lokesh.ranjan.phe23@itbhu.ac.in>
2025-07-09 19:21:29 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+const allowedOrigins = [
+  "http://localhost:5173",                 // Local dev
+  "https://your-vercel-frontend.vercel.app" // Your Vercel URL
+];
 import express  from "express";
 import cors from "cors";
 
 const app= express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
 // eslint-disable-next-line no-undef
 const PORT= process.env.PORT || 3000;
