06e023ecfb6abbb8ea2b8e41f681c498339f04fc
LokeshRanjan <lokesh.ranjan.phe23@itbhu.ac.in>
2025-11-26 01:30:35 +0530

    Docker works fine

--- server/index.js
+++ server/index.js
@@ -20,21 +20,10 @@ app.use(cors({
 
 app.use(express.json());
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.includes(origin)) {
-//       return callback(null, true);
-//     } else {
-//       return callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true
-// }));
 
 
 // eslint-disable-next-line no-undef
-const PORT= process.env.PORT || 3000;
+const PORT= process.env.PORT || 5000;
 import fetchcontest from './fetchcontest.js';
 import fetchLeetCodeContests from "./leetcode.js";
 import authRoutes from './routes/auth.js';
