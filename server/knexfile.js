70c9f181186342e4af799a66739f69fa6387aebb
LokeshRanjan <lokesh.ranjan.phe23@itbhu.ac.in>
2025-12-15 17:41:51 +0530

    env var

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,11 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const development = {
     client: "pg",
     connection: {
         host: "localhost",
         port: 5432,
-        user: "postgres",
-        password: "postgres",
-        database: "mydb"
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DBNAME ,
     },
     migrations: {
         directory: "./migrations"
